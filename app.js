eab7e82ea4026caae0b1fdefc065ad1551174cb1
Prince Verma <princeverma360p@gmail.com>
2022-11-10 17:16:01 +0530

    added cors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const navBarRouter = require('./routes/navbar')
-const newsRouter = require('./routes/news')
-const latestEvents = require('./routes/latestEvent')
-const administrationRouter = require('./routes/administration')
-const noticeRouter = require('./routes/notice')
-const rankingRouter = require('./routes/ranking')
-const reasearchHighlights = require('./routes/researchHighlights')
-const photoGalleryRouter = require('./routes/photoGallery')
-const footerRouter = require('./routes/footer')
-const aboutRouter = require('./routes/about')
+const navBarRouter = require("./routes/navbar");
+const newsRouter = require("./routes/news");
+const latestEvents = require("./routes/latestEvent");
+const administrationRouter = require("./routes/administration");
+const noticeRouter = require("./routes/notice");
+const rankingRouter = require("./routes/ranking");
+const reasearchHighlights = require("./routes/researchHighlights");
+const photoGalleryRouter = require("./routes/photoGallery");
+const footerRouter = require("./routes/footer");
+const aboutRouter = require("./routes/about");
 
 //----------------------------------->
 
@@ -18,7 +19,11 @@ const app = express();
 
 //middleware
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 //routes
 app.use("/navbar", navBarRouter);
 app.use("/news", newsRouter);
@@ -30,7 +35,6 @@ app.use("/researchHighlights", reasearchHighlights);
 app.use("/photoGallery", photoGalleryRouter);
 app.use("/footer", footerRouter);
 app.use("/about", aboutRouter);
-
 
 //Export----------------------------->
 module.exports = app;
