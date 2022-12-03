78a9f11c4c0e040dc43feab833d9644ec3a56d0b
Prince Verma <princeverma360p@gmail.com>
2022-12-03 14:44:33 +0530

    cors settling

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const cors=require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const navBarRouter = require('./routes/navbar')
-const newsRouter = require('./routes/news')
-const latestEvents = require('./routes/latestEvent')
-const administrationRouter = require('./routes/administration')
-const noticeRouter = require('./routes/notice')
-const rankingRouter = require('./routes/ranking')
-const reasearchHighlights = require('./routes/researchHighlights')
-const photoGalleryRouter = require('./routes/photoGallery')
-const footerRouter = require('./routes/footer')
-const aboutRouter = require('./routes/about')
-const bodyParser=require('body-parser');
+const navBarRouter = require("./routes/navbar");
+const newsRouter = require("./routes/news");
+const latestEvents = require("./routes/latestEvent");
+const administrationRouter = require("./routes/administration");
+const noticeRouter = require("./routes/notice");
+const rankingRouter = require("./routes/ranking");
+const reasearchHighlights = require("./routes/researchHighlights");
+const photoGalleryRouter = require("./routes/photoGallery");
+const footerRouter = require("./routes/footer");
+const aboutRouter = require("./routes/about");
+const bodyParser = require("body-parser");
 //----------------------------------->
 
 //initialize app
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(express.json());
-bodyParser.urlencoded({extended:true});
+bodyParser.urlencoded({ extended: true });
 app.use(bodyParser.json());
-
 
 //routes
 app.use("/navbar", navBarRouter);
