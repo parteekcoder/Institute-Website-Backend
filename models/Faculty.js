c2ef21c590fad8b16ec981da1a3e6a83841b464a
Aditya Garg <uber6707@gmail.com>
2023-02-26 02:38:43 +0530

    Updated new schemas

--- models/Faculty.js
+++ models/Faculty.js
@@ -16,6 +16,61 @@ const Schema=new mongoose.Schema({
     position:{
         type:String
     },
+    gender: {
+        type: String,
+        enum: ['male','female','other']
+    },
+    dob: {
+        type: Date,
+        default: new Date().now()
+    },
+    father_name: {
+        type: String
+    },
+    designation: {
+        type: String
+    },
+    nationality: {
+        type: String
+    },
+    publications:{
+        type: Array,
+        default: []
+        // Publisher, Authors, ISBN/ISSN, Type, Year, Action
+    },
+    affiliations:{
+        type: Array,
+        default: []
+        // Designation and Organisation
+    },
+    affiliations:{
+        type: Array,
+        default: []
+        // Conference, Publication, Publication Link
+    },
+    research_profile: {
+        type:Array,
+        default: []
+        // Research Interests, Brief Research Profile
+    },
+    research_project: {
+        type:Array,
+        default: []
+        // Role, Project Type, Title, Funding Agency, From, To, Amount, Status, Co-investigator
+    },
+    personal_link: {
+        type: URL,
+    },
+    journal: {
+        type:Array,
+        default: []
+        // Year, Journal, Publication, and Publication Link
+    },
+    event: {
+        type:Array,
+        default: []
+        // category, type, title, venue, from, to, designation
+    },
     
     sourceOfInfo: {
         type: Object,
